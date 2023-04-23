38130e0aaf4b12894f7a1967b7943aabb3043026
iVGeek <ianvincent916@gmail.com>
2023-04-23 10:51:46 -0700

    update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
-
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """view function that displays all states currently in storage"""
+    all_states = storage.all("State").values()
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    '''The cities_by_states page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    for state in all_states:
-        state.cities.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('8-cities_by_states.html', **ctxt)
+    """view function that displays all cities currently in storage"""
+    all_states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', all_states=all_states)
 
 
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def teardown(self):
+    """function that removes the current SQLAlchemy Session"""
     storage.close()
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-    
-    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
